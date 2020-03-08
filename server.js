1599740c965bbba88a81228bf141b58ddcadf591
Joseph Nganga <kajoengasa@gmail.com>
2020-03-08 16:25:31 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+
+const app = express();
+
+app.enable("trust proxy");
+//here we are configuring dist to serve app files
+app.use("/", function(req, res, next) {
+  if (req.protocol !== "https") {
+    var secureUrl = "https://" + req.headers["host"] + req.url;
+    res.writeHead(301, { Location: secureUrl });
+    res.end();
+  }
+  next();
+});
+
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+//console.log(`app is listening on port: ${port}`);
